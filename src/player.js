7b2d1684e2cb24706a4d07c5b4e59531d3f6876f
Daniel Hones <danielhones@gmail.com>
2016-08-21 03:52:12 -0400

    added key bindings and inputs for loop stuff, renamed wavesurfer variable to ws on AudioPlayer class

--- src/player.js
+++ src/player.js
@@ -1,8 +1,17 @@
 var WAVEFORM_CONTAINER = '#waveform';
+var SMALL_SCRUB_INCREMENT = 0.1;  // seconds
+var LARGE_SCRUB_INCREMENT = 2;  // seconds
 
 
 function pad(n) {
     return (0 <= n && n < 10) ? ("0" + n) : n.toString();
+}
+
+
+function format_time(time) {
+    var minutes = Math.floor(time / 60);
+    var seconds = (time - minutes * 60).toFixed(3);
+    return minutes + ":" + pad(seconds);
 }
 
 
@@ -14,44 +23,64 @@ var AudioPlayer = function() {
     this.loop_enabled = false;
 
     this.init = function () {
-	that.wavesurfer = WaveSurfer.create({
+	that.ws = WaveSurfer.create({
 	    container: WAVEFORM_CONTAINER,
 	    height: 128,
 	    normalize: true,
 	    progressColor: 'darkblue',
 	    waveColor: 'blue'
 	});
-	that.wavesurfer.on('audioprocess', that.handle_playing);
-	that.wavesurfer.on('seek', that.update_time);
-	that.wavesurfer.on('ready', function() { that.update_time(0); });
-	that.wavesurfer.load('../untitled.mp3');
-	that.wavesurfer.on('ready', function() {
-	    that.loop_end = that.wavesurfer.getDuration() / 2;
+	that.ws.on('audioprocess', that.handle_playing);
+	that.ws.on('seek', function() { that.update_time(); } );
+	that.ws.on('ready', function() { that.update_time(0); });
+	that.ws.load('../untitled.mp3');
+	that.ws.on('ready', function() {
+	    that.loop_end = that.ws.getDuration() / 2;
 	});
     };
     
     this.update_time = function(time) {
-	time = time || that.wavesurfer.getCurrentTime();
-	var minutes = Math.floor(time / 60);
-	var seconds = (time - minutes * 60).toFixed(3);
-	document.getElementById('time').innerHTML = minutes + ":" + pad(seconds);
+	time = time || that.ws.getCurrentTime();
+	document.getElementById('time').innerHTML = format_time(time);
     };
 
     this.handle_playing = function() {
-	if (that.loop_enabled && that.wavesurfer.getCurrentTime() >= that.loop_end) {
+	if (that.loop_enabled && that.ws.getCurrentTime() >= that.loop_end) {
 	    setTimeout(function () {
-                that.wavesurfer.seekTo(seconds_to_progress(that.loop_start));
+                that.ws.seekTo(seconds_to_progress(that.loop_start));
             }, 0);
 	}
 	that.update_time();
     };
 
     this.set_loop_start = function(time) {
-	that.loop_start = time || that.wavesurfer.getCurrentTime();
+	that.loop_start = time || that.ws.getCurrentTime();
+	if (that.loop_end < that.loop_start) {
+	    that.loop_end = that.loop_start;
+	}
+	if (that.loop_start < 0) {
+	    that.loop_start = 0;
+	}
+	if (that.loop_start > that.ws.getDuration()) {
+	    that.loop_start = that.ws.getDuration();
+	}
+	document.getElementById('loop_start_input')
+	    .value = format_time(that.loop_start);
     };
 
     this.set_loop_end = function(time) {
-	that.loop_end = time || that.wavesurfer.getCurrentTime();
+	that.loop_end = time || that.ws.getCurrentTime();
+	if (that.loop_end < that.loop_start) {
+	    that.loop_start = that.loop_end;
+	}
+	if (that.loop_end < 0) {
+	    that.loop_end = 0;
+	}
+	if (that.loop_end > that.ws.getDuration()) {
+	    that.loop_end = that.ws.getDuration();
+	}
+	document.getElementById('loop_end_input')
+	    .value = format_time(that.loop_end);
     };
 
     this.loop_on = function() {
@@ -63,27 +92,48 @@ var AudioPlayer = function() {
     };
     
     this.dispatch_keypress = function(e) {
+	// TODO: make this not so dumb
 	if (e.key === " ") {
-	    that.wavesurfer.playPause();
+	    that.ws.playPause();
 	} else if (e.key === "h") {
-	    /*
-	      TODO: these skip values should depend on the zoom level.
-	      Zoomed farther out, they should have a larger effect,
-	      up to a max.  Zoomed in they can have more minute control.
-	    */
-	    that.wavesurfer.skip(-2);
+	    that.ws.skip(-2);
 	} else if (e.key === "j") {
-	    that.wavesurfer.skip(-0.1);
+	    that.ws.skip(-0.1);
 	} else if (e.key === "k") {
-	    that.wavesurfer.skip(0.1);
+	    that.ws.skip(0.1);
 	} else if (e.key === "l") {
-	    that.wavesurfer.skip(2);
+	    that.ws.skip(2);
 	} else if (e.key === "+") {
 	    that.zoom_in();
 	} else if (e.key === "_") {
 	    that.zoom_out();
 	} else if (e.key === ")") {
-	    that.wavesurfer.zoom(0);
+	    that.ws.zoom(0);
+	} else if (e.key === "u") {
+	    // set loop start to current time
+	    that.set_loop_start(that.ws.getCurrentTime());
+	} else if (e.key === "i") {
+	    // set loop end to current time
+	    that.set_loop_end(that.ws.getCurrentTime());
+	} else if (e.key === "Y") {
+	    // scrub loop start left
+	    that.set_loop_start(that.loop_start - SMALL_SCRUB_INCREMENT);
+	} else if (e.key === "U") {
+	    // scrub loop start right
+	    that.set_loop_start(that.loop_start + SMALL_SCRUB_INCREMENT);
+	} else if (e.key === "I") {
+	    // scrub loop end left
+	    that.set_loop_end(that.loop_end - SMALL_SCRUB_INCREMENT);
+	} else if (e.key === "O") {
+	    // scrub loop end right
+	    that.set_loop_end(that.loop_end + SMALL_SCRUB_INCREMENT);
+	} else if (e.key === "o") {
+	    that.loop_enabled ? that.loop_off() : that.loop_on();
+	    document.getElementById('loop_enabled')
+		.innerHTML = that.loop_enabled ? "loop enabled" : "no loop";
+	} else if (e.key === "s") {
+	    // toggle scrollParent property
+	    that.ws.params.scrollParent ? (that.ws.params.scrollParent = false) : (that.ws.scrollParent = true);
 	}
     };
 
@@ -92,11 +142,11 @@ var AudioPlayer = function() {
     };
 
     this.zoom_in = function() {
-	that.wavesurfer.zoom(that.wavesurfer.params.minPxPerSec + 4);
+	that.ws.zoom(that.ws.params.minPxPerSec + 4);
     };
 
     this.zoom_out = function() {
-	that.wavesurfer.zoom(that.wavesurfer.params.minPxPerSec - 4);
+	that.ws.zoom(that.ws.params.minPxPerSec - 4);
     };
 
     this.load_audio_file = function(e) {
@@ -105,11 +155,11 @@ var AudioPlayer = function() {
 	if (!file) {
 	    return;
 	}
-	that.wavesurfer.loadBlob(file);
+	that.ws.loadBlob(file);
     };
 
     function seconds_to_progress(seconds) {
-	return seconds / that.wavesurfer.getDuration();
+	return seconds / that.ws.getDuration();
     }
 };
 
@@ -123,9 +173,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById('waveform')
     	.addEventListener('mousemove', player.dispatch_mouse);
 
-    document.getElementById('skip_back_btn').onclick = player.wavesurfer.skipBackward;
-    document.getElementById('play_pause_btn').onclick = player.wavesurfer.playPause;
-    document.getElementById('skip_forward_btn').onclick = player.wavesurfer.skipForward;
+    document.getElementById('skip_back_btn').onclick = player.ws.skipBackward;
+    document.getElementById('play_pause_btn').onclick = player.ws.playPause;
+    document.getElementById('skip_forward_btn').onclick = player.ws.skipForward;
     document.getElementById('zoom_in_btn').onclick = player.zoom_in;
     document.getElementById('zoom_out_btn').onclick = player.zoom_out;
 });
